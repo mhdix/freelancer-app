946993ce45f230edfd314b395d254ab4b234142c
mhdix <domanloomu@gmail.com>
2025-07-29 11:50:57 +0330

    feature: add package and configure react router dom

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import Auth from "./components/Auth/Auth";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/auth" element={<Auth />} />
+    </Routes>
+  );
+}
+
+export default App;
